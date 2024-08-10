254bcb227dbc5421df79aae302e3d62fa45baa95
Gaby Macedo <135060010+GabyyMacedo@users.noreply.github.com>
2024-08-10 11:35:55 -0300

    Update __init__.py

--- eclipse/eclipse/__init__.py
+++ eclipse/eclipse/__init__.py
@@ -1,8 +1,6 @@
 from flask import Flask
-from controllers import tarefa_controller
-
+from .controllers import tarefa_controllers
 app = Flask(__name__)
-app.register_blueprint(tarefa_controller)
-
-if __name__ == "__main__":
+app.register_blueprint(tarefa_controllers)
+if __name__=="__main__":
     app.run(debug=True)
