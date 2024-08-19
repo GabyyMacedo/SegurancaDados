a18101a8c3985b361cd7cae1c98c8cac853a6f04
Gaby Macedo <135060010+GabyyMacedo@users.noreply.github.com>
2024-08-19 18:33:33 -0300

    Delete eclipse directory

--- eclipse/eclipse/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-from .controllers import tarefa_controllers
-app = Flask(__name__)
-app.register_blueprint(tarefa_controllers)
-if __name__=="__main__":
-    app.run(debug=True)
